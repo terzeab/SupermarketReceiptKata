ad68668e388d9752e556589a7afa51bab4358629
Bogdan Terzea <bogdan.t@mondly.com>
2024-08-01 15:34:09 +0200

    Init project - 10 min work

---
+++ src/Service/CartService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+class CartService
+{
+    public function calculateBasket(array $basket): float
+    {
+        //TODO: implement this
+        return 1;
+    }
+}
